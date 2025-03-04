f9f3174b290fc0d4fa215296194c37cc652d39fd
Tarcio2020 <tarcioapps7@gmail.com>
2025-03-04 11:35:03 -0300

    first commit

---
+++ human-resources/human-resources/src/test/java/com/tarcioteles/human_resources/HumanResourcesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tarcioteles.human_resources;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HumanResourcesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
